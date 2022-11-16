05268fb6e9c598a26362d2310b9be497bde771d1
meadeconway <meadeconway@gmail.com>
2022-11-16 18:40:51 +0100

    Initial commit after getting the final version working + making personal tweaks

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ReactWines
 A short React project made for reviewing wines.
+
+Made alongside the book React: Up & Running by Stoyan Stefanov.
---
+++ src/components/Logo.js
@@ -0,0 +1,7 @@
+import logo from './static/css/images/my_wine_logo.png';
+
+const Logo = () => {
+  return <img src={logo} height="150" alt="My Wine logo" />;
+};
+
+export default Logo;
